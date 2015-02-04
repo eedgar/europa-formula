431437a3e47adadb607e9c512de64426541a9b1e
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-02-04 17:15:11 -0600

    Now uses the right $HOME

--- packer/scripts/users.sh
+++ packer/scripts/users.sh
@@ -2,8 +2,8 @@ groupadd docker
 groupadd zenoss --gid 1206
 groupadd awsadmins --gid 1001
 
-useradd zenoss -b /home/zenoss --uid 1337 --gid 1206 --groups sudo,docker,awsadmins --shell /bin/bash
+useradd zenoss --uid 1337 --gid 1206 --groups sudo,docker,awsadmins -s /bin/bash
 echo "zenoss:zenoss" | chpasswd
 mkdir -p /home/zenoss/.ssh
-chowner zenoss:zenoss /home/zenoss/.ssh
+chown zenoss:zenoss /home/zenoss/.ssh
 chmod 700 /home/zenoss/.ssh
