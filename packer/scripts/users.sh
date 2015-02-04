e1fa687b3c89754def5d7bd0ab9a6b0a3c61f355
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-02-04 11:44:55 -0600

    Updated for zenoss user

--- packer/scripts/users.sh
+++ packer/scripts/users.sh
@@ -3,6 +3,7 @@ groupadd zenoss --gid 1206
 groupadd awsadmins --gid 1001
 
 useradd zenoss -b /home/zenoss --uid 1337 --gid 1206 --groups sudo,docker,awsadmins --shell /bin/bash
+echo "zenoss:zenoss" | chpasswd
 mkdir -p /home/zenoss/.ssh
 chowner zenoss:zenoss /home/zenoss/.ssh
 chmod 700 /home/zenoss/.ssh
